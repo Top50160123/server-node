8259fda8660874ed4bfe48f38956fc7990ed7c29
Top50160123 <thitikawja9@gmail.com>
2024-03-06 21:15:07 +0700

    fix: create pdf

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// firebase
 const serviceAccount = require("./firebase.json");
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -17,88 +18,10 @@ admin.initializeApp({
 });
 const bucket = admin.storage().bucket();
 
-const randomBytes = crypto.randomBytes(32);
-const yourSecretKey = randomBytes.toString("hex");
+// api /
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.options("/api/get-secret-key", cors());
-app.get("/api/get-secret-key", (req, res) => {
-  res.json({ secretKey: yourSecretKey });
-});
-
-app.options("/api/generate-otp-and-qrcode", cors());
-app.post("/api/generate-otp-and-qrcode", async (req, res) => {
-  try {
-    const secret = speakeasy.generateSecret({
-      name: "OTP Zero-Trust : project",
-    });
-    const otpauth_url = secret.otpauth_url;
-    const qrCodeDataURL = await QRCode.toDataURL(otpauth_url);
-
-    res.json({
-      secret: secret.base32,
-      otpauth_url,
-      qrcode: qrCodeDataURL,
-    });
-  } catch (error) {
-    console.error("Error generating QR code:", error);
-    res.status(500).json({ error: "Error generating QR code" });
-  }
-});
-
-app.options("/api/verify-otp", cors());
-app.post("/api/verify-otp", (req, res) => {
-  try {
-    const { otp, secret } = req.body;
-    const isValid = speakeasy.totp.verify({
-      secret,
-      encoding: "base32",
-      token: otp,
-      window: 2,
-    });
-
-    if (isValid) {
-      res.json({ message: "OTP is valid" });
-    } else {
-      res.status(401).json({ error: "Invalid OTP" });
-    }
-  } catch (error) {
-    console.error("Error verifying OTP:", error);
-    res.status(500).json({ error: "Error verifying OTP" });
-  }
-});
-
-app.options("/getToken/:code", cors());
-app.post("/getToken/:code", async (req, res) => {
-  try {
-    const codeFromURL = req.params.code;
-    const requestData = {
-      code: codeFromURL,
-      redirect_uri: "https://final-project-eta-ruby.vercel.app/callback",
-      client_id: "dBH4CNbDdruZ8qyD3qqubEYdVz5xvpnqsDe7yrQb",
-      client_secret: "tYEyZQnjDzQ11j8JQDjdTQh0deHEkAfNKnaqaArf",
-      grant_type: "authorization_code",
-    };
-
-    console.log("codeFromURL:", codeFromURL);
-
-    const headers = {
-      "Content-Type": "application/x-www-form-urlencoded",
-    };
-
-    const response = await axios.post(
-      "https://oauth.cmu.ac.th/v1/GetToken.aspx",
-      querystring.stringify(requestData),
-      { headers }
-    );
-
-    console.log("Response:", response.data);
-    res.status(200).json(response.data);
-  } catch (error) {
-    console.error("Error:", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
+// api create pdf
 app.options("/generate-pdf", cors());
 app.post("/generate-pdf", async (req, res) => {
   try {
@@ -133,23 +56,6 @@ app.post("/generate-pdf", async (req, res) => {
   }
 });
 
-app.options("/api/delete-pdf/:fileName", cors());
-app.delete("/api/delete-pdf/:fileName", async (req, res) => {
-  try {
-    const fileName = req.params.fileName;
-    console.log(`${fileName}.pdf`);
-    await bucket.file(`${fileName}.pdf`).delete();
-    res.status(200).json({ message: "PDF deleted successfully" });
-  } catch (error) {
-    console.error("Error deleting PDF:", error);
-    res.status(500).json({ error: "An error occurred while deleting the PDF" });
-  }
-});
-
-app.get("/", (req, res) => {
-  res.send("Welcome, your app is working well");
-});
-
-app.listen(5004, () => console.log("Server listening on port 5004"));
+app.listen(5004, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
